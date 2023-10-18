65803cdcb2a57ac58208970bd7e3cceff8038c4e
Kavyasree14 <kavyasreepolavarapu@gmail.com>
2023-10-17 21:54:45 -0700

    controls screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
 
     public void QuitGame()
@@ -32,6 +32,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Levels");
         Debug.Log("Levels");
+    }
+    public void controls()
+    {
+        SceneManager.LoadScene("Controls");
     }
 
 }
