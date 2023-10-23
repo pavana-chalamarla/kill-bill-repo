8873db6a394d62b0acd424f8e2cadca1e418c384
Kavyasree14 <kavyasreepolavarapu@gmail.com>
2023-10-23 14:52:48 -0700

    Removed bugs in falling spike mechanic

--- Assets/Scripts/FallingSpike.cs
+++ Assets/Scripts/FallingSpike.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class FallingSpike : MonoBehaviour
 {
     public float fallSpeed = 5.0f;
+    public float resetDelay = 2.0f; // Time delay before resetting
+    public Vector3 initialPosition; // Initial position of the spike
     public float groundLevel = 0.0f;
 
     private bool hasHitGround = false;
+    private float timeSinceHitGround = 0f;
+
+    private void Start()
+    {
+        // Store the initial position of the spike
+        initialPosition = transform.position;
+    }
 
     private void Update()
     {
@@ -25,5 +34,27 @@ public class FallingSpike : MonoBehaviour
                 // Optionally, you can play a sound or trigger an event when the spike hits the ground.
             }
         }
+        else
+        {
+            // Spike has hit the ground, start a timer
+            timeSinceHitGround += Time.deltaTime;
+
+            // Check if the reset delay time has passed
+            if (timeSinceHitGround >= resetDelay)
+            {
+                // Reset the spike's position
+                ResetSpikePosition();
+            }
+        }
+    }
+
+    void ResetSpikePosition()
+    {
+        // Reset the position of the spike to its initial position
+        transform.position = initialPosition;
+
+        // Reset state variables
+        hasHitGround = false;
+        timeSinceHitGround = 0f;
     }
 }
