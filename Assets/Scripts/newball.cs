1a46df56000f36f68215ecd5a6b82773fdb65cc9
pavana-chalamarla <chalamar@usc.edu>
2023-11-21 00:27:20 -0800

    changes hard level

---
+++ Assets/Scripts/newball.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newball : MonoBehaviour
+{
+
+    [SerializeField] private Transform originalBall; // Reference to the original ball's Transform
+    private Transform mirrorTransform;
+    public Ballmovement mirrorBallMovement;
+    private bool shouldReverse = true;
+    public bool grounded;
+
+    void Awake()
+    {
+        mirrorTransform = transform; // Get the Transform of the mirror object
+        mirrorBallMovement = mirrorTransform.GetComponent<Ballmovement>();
+        if (originalBall == null)
+        {
+            Debug.LogError("originalBall is not assigned in the Inspector!");
+        }
+    }
+
+
+    void LateUpdate()
+    {
+        float directionMultiplier = shouldReverse ? -1 : 1;
+        float horizontal = directionMultiplier * originalBall.GetComponent<Ballmovement>().horizontal;
+        mirrorBallMovement.horizontal = horizontal;
+
+        // Mirror the jump movement (opposite jump)
+        if (Input.GetButtonDown("Jump") && originalBall.GetComponent<Ballmovement>().IsGrounded() && grounded)
+        {
+            mirrorBallMovement.rb.velocity = new Vector2(mirrorBallMovement.rb.velocity.x, mirrorBallMovement.jump);
+            grounded = false;
+        }
+      
+    }
+
+    public void ToggleDirection()
+    {
+        //Debug.Log("working");
+        shouldReverse = !shouldReverse; // Toggle the direction
+        LateUpdate();
+
+    }
+
+     public void OnCollisionEnter2D(Collision2D other){
+        Debug.Log("ground collision");
+        if(other.gameObject.layer==LayerMask.NameToLayer("Ground")){
+            grounded = true;
+        }
+    }
+}
